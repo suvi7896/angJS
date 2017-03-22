a830ef8a32a149bc8df8228ab355d3e602670572
Suvigya <suvi7896@gmail.com>
2017-03-23 00:13:41 +0530

    NameCalculator added

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ angular.module('myFirstApp', [])
 .controller('MyFirstController',function ($scope)
 {
 $scope.name = "Suvigya";
+
+$scope.sayHello = function () {
+  return "Hello Fellas!";
+};
 });
 
 
